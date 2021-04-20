e5e4c8763f19c94ff65e4b1a15af0832d6b8e074
PnzJust <32747902+PnzJust@users.noreply.github.com>
2021-04-20 22:21:49 +0300

    Update input.cpp

--- input.cpp
+++ input.cpp
@@ -1,10 +1,31 @@
 int main(){
     int a;
+    bool x;
     if(a){
         int b;
     }
 
+    if (true)
+    {
+        return 0;
+    }
+
+    if (3 == 4) {
+        return 4;
+    }
+    if (1 - 1 != 2) return 0;
+
+    if (1 + 1 == 2) { return 0;}
+
     while(true){
         int c;
     }
+
+    while(false)
+        break;
+
+    while (1 ^ 1 > 2) break;
+
+
+    while (x != false) { break;}
 }
